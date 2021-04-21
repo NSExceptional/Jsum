e6ceefcbc45542fd5690d310e8153752111d8778
Tanner Bennett <tannerbennett@me.com>
2021-04-21 16:49:28 -0500

    Add more tests

--- Tests/JsumTests.swift
+++ Tests/JsumTests.swift
@@ -24,8 +24,64 @@ class JsumTests: XCTestCase {
         _ = try Transformer<Int, String>.transform(5)
         _ = try Transformer<[Int], [String]>.transform([5])
         
-        let person: (name: String, age: Int) = try Jsum.decode(
+        let _: (name: String, age: Int) = try Jsum.decode(
             from: ["name": "Bob", "age": 25]
         )
+    }
+    
+    func testBuiltinMetadata() {
+        let metadata = KnownMetadata.Builtin.int64
+        XCTAssert(metadata.isBuiltin)
+        
+        let intm = reflect(Int.self)
+        XCTAssert(intm.isBuiltin)
+        
+        let stringm = reflect(String.self)
+        XCTAssert(!stringm.isBuiltin)
+    }
+    
+    func testEmptyStruct() {
+        struct Nothing { }
+        
+        let metadata = reflectStruct(Nothing.self)!
+        XCTAssert(metadata.fields.isEmpty)
+        XCTAssert(metadata.vwt.size == 0)
+    }
+    
+//    func testEnumTypeNoFields() {
+//        let metadata = reflectEnum(JSON.self)!
+//        XCTAssert(metadata.descriptor.fields.records.first!.flags.isVar)
+//    }
+    
+    func testFieldedTypeHasNoComputedFields() {
+        let metadata = reflectClass(Person.self)!
+        let fields = metadata.descriptor.fields
+        
+        XCTAssertEqual(fields.records.count, metadata.fieldOffsets.count)
+        XCTAssert(fields.records.filter({ $0.name == "tuple" }).isEmpty)
+    }
+
+    func testProtocolConformances() {
+        let person = reflectClass(Person.self)!
+        XCTAssert(person.conforms(to: Conformable.self))
+    }
+    
+    func testBuiltinDecodeProtocolMethod() throws {
+        let int = reflectStruct(Int.self)!
+        XCTAssert(int.conforms(to: JSONCodable.self))
+        
+        XCTAssertEqual(try Jsum.decode(from: 5), 5)
+        XCTAssertEqual(try Jsum.decode(from: 3.14159), 3.14159)
+    }
+    
+    func testAllocObject() {
+        let expect = XCTestExpectation(description: "deinit")
+        let cls = reflectClass(JustDeallocateMe.self)!
+        DispatchQueue.global().async {
+            var obj: JustDeallocateMe? = cls.createInstance(props: ["expectation": expect])
+            obj = nil
+        }
+        
+        self.wait(for: [expect], timeout: 2)
     }
 }
