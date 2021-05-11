044c020e03a85b9755d2c750e37fcde2c7042a54
Tanner Bennett <tannerbennett@me.com>
2021-05-11 12:04:11 -0500

    More test helpers

--- Tests/TestHelpers.swift
+++ Tests/TestHelpers.swift
@@ -1,5 +1,5 @@
 //
-//  Date+Testing.swift
+//  TestHelpers.swift
 //  JsumTests
 //
 //  Created by Tanner Bennett on 5/11/21.
@@ -15,5 +15,23 @@ extension Date {
     var ignoringTime: Date {
         let comps = Calendar.current.dateComponents([.day, .year], from: self)
         return Calendar.current.date(from: comps)!
+    }
+}
+
+extension Result {
+    var failed: Bool {
+        if case .failure(_) = self {
+            return true
+        }
+        
+        return false
+    }
+    
+    var succeeded: Bool {
+        if case .failure(_) = self {
+            return false
+        }
+        
+        return true
     }
 }
