7fd485c4f12e7ba13212d2bc6329fb61b12053f5
Tanner Bennett <tannerbennett@me.com>
2025-01-12 14:09:55 -0600

    Target iOS 13
    iOS 12.2 is when Swift started being bundled with the OS

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Jsum",
-    platforms: [.macOS(.v10_15), .iOS(.v12)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         .library(
             name: "Jsum",
